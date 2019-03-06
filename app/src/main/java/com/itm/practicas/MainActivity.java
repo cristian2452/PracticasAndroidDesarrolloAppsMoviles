94a54df85ac1fc22a90bfcf2b961c089babca3f2
chon <cristian2452@gmail.com>
2019-03-06 07:20:54 -0800

    added log d message

--- app/src/main/java/com/itm/practicas/MainActivity.java
+++ app/src/main/java/com/itm/practicas/MainActivity.java
@@ -2,12 +2,16 @@ package com.itm.practicas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Log.d("message","printing a message in the console");
     }
 }
