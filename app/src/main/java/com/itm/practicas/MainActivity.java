1f7602ed4a77ce338dcdbc0e54885a30cd458cb0
chon <cristian2452@gmail.com>
2019-03-11 09:57:53 -0700

    Creado imageView que cambia de imagen al rotar el dispositivo

--- app/src/main/java/com/itm/practicas/MainActivity.java
+++ app/src/main/java/com/itm/practicas/MainActivity.java
@@ -3,6 +3,7 @@ package com.itm.practicas;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,13 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView text;
-        text = findViewById(R.id.TextView);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-            text.setText("Vertical");
+            imageView.setImageResource(R.drawable.android);
         }else if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            text.setText("Horizontal"); 
+            imageView.setImageResource(R.drawable.google);
         }
     }
 }
