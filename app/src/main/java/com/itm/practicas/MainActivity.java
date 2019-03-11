ac5aeac93abd5580a6d67da7a03e03cbe34cda7e
chon <cristian2452@gmail.com>
2019-03-11 07:38:26 -0700

    Creado los metodos de ciclo de vida de la aplicacion

--- app/src/main/java/com/itm/practicas/MainActivity.java
+++ app/src/main/java/com/itm/practicas/MainActivity.java
@@ -2,6 +2,7 @@ package com.itm.practicas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Toast de onCreate", Toast.LENGTH_LONG).show();
     }
+
+    @Override protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Toast de onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Toast de onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onPause() {
+        Toast.makeText(this, "Toast de onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override protected void onStop() {
+        Toast.makeText(this, "Toast de onStop", Toast.LENGTH_SHORT).show();
+        super.onStop();
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Toast de onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onDestroy() {
+        Toast.makeText(this, "Toast de onDestroy", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
 }
